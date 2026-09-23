0b2040d2d75493cca3fa0a85c0f4f791e232851d
trikun <kun.code.1311@gmail.com>
2026-09-23 23:45:35 +0700

    Add personal photos of Kim Thi into wish popup

--- assets/script.js
+++ assets/script.js
@@ -366,31 +366,31 @@ const interactiveObjects = [];
 const wishList = [
   {
     text: "Chúc Kim Thi một mùa Trung Thu siêu cấp đáng yêu, ăn bánh thả ga mà không sợ béo nha! 🥮✨",
-    img: "./assets/1.jpg",
+    img: "./assets/kt1.png",
   },
   {
     text: "Cầu chúc cho mọi điều ước bé xíu xiu dễ thương của Kim Thi đều sớm thành hiện thực nè! 🐰⭐",
-    img: "./assets/2.jpg",
+    img: "./assets/kt2.png",
   },
   {
     text: "Chúc Kim Thi lúc nào cũng vui tươi, rạng rỡ và tràn đầy năng lượng tích cực như ánh trăng rằm! 🌕🥰",
-    img: "./assets/3.jpg",
+    img: "./assets/kt3.png",
   },
   {
     text: "Trung Thu vui vẻ nha Kim Thi! Chúc bạn làm gì cũng suôn sẻ, việc gì cũng hanh thông và thuận lợi! 🎉🎈",
-    img: "./assets/1.jpg",
+    img: "./assets/kt4.png",
   },
   {
     text: "Mong Kim Thi mỗi ngày mở mắt ra đều gặp toàn chuyện vui và những điều ngọt ngào, dễ thương! 🌸✨",
-    img: "./assets/2.jpg",
+    img: "./assets/kt5.png",
   },
   {
     text: "Chúc Kim Thi có một đêm Trung Thu thật chill, ngắm trăng phá cỗ và luôn giữ nụ cười thật tươi nha! 🏮😄",
-    img: "./assets/3.jpg",
+    img: "./assets/kt1.png",
   },
   {
     text: "Chúc Kim Thi luôn xinh xắn, an nhiên, hạnh phúc và có một mùa trăng thật ấm áp bên gia đình và bạn bè! 🎊💖",
-    img: "./assets/1.jpg",
+    img: "./assets/kt3.png",
   },
 ];
 
