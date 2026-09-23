20ad04a67f64afebe379c118c0df43192601ecb9
trikun <kun.code.1311@gmail.com>
2026-09-24 00:22:21 +0700

    Chinh sua loi chinh ta va cau tu loi chuc Kim Thi chuan tieng Viet tu nhien

--- assets/script.js
+++ assets/script.js
@@ -374,11 +374,11 @@ const interactiveObjects = [];
 
 const wishList = [
   {
-    text: "Chúc Kim Thi một mùa Trung Thu siêu cấp đáng yêu, ăn bánh thả ga mà không sợ béo nha! 🥮✨",
+    text: "Chúc Kim Thi một mùa Trung Thu thật vui vẻ và đáng yêu, ăn bánh thả ga mà không lo béo nha! 🥮✨",
     img: "./assets/kt1.png",
   },
   {
-    text: "Cầu chúc cho mọi điều ước bé xíu xiu dễ thương của Kim Thi đều sớm thành hiện thực nè! 🐰⭐",
+    text: "Cầu chúc cho mọi điều ước nhỏ bé và dễ thương của Kim Thi đều sớm trở thành hiện thực nhé! 🐰⭐",
     img: "./assets/kt2.png",
   },
   {
@@ -386,31 +386,31 @@ const wishList = [
     img: "./assets/kt3.png",
   },
   {
-    text: "Trung Thu vui vẻ nha Kim Thi! Chúc bạn làm gì cũng suôn sẻ, việc gì cũng hanh thông và thuận lợi! 🎉🎈",
+    text: "Trung Thu vui vẻ nha Kim Thi! Chúc bạn làm việc gì cũng suôn sẻ, vạn sự hanh thông và luôn gặp may mắn! 🎉🎈",
     img: "./assets/kt4.png",
   },
   {
-    text: "Mong Kim Thi mỗi ngày mở mắt ra đều gặp toàn chuyện vui và những điều ngọt ngào, dễ thương! 🌸✨",
+    text: "Mong Kim Thi mỗi ngày trôi qua đều ngập tràn niềm vui cùng những điều ngọt ngào, dễ thương! 🌸✨",
     img: "./assets/kt5.png",
   },
   {
-    text: "Chúc cô gái Kim Thi mùa trăng này luôn ngập tràn may mắn, niềm vui và vạn sự như ý! 🎂🎈",
+    text: "Chúc Kim Thi mùa trăng này luôn ngập tràn may mắn, niềm vui và vạn sự như ý! 🥮🏮",
     img: "./assets/kt6.png",
   },
   {
-    text: "Chúc Kim Thi lúc nào cũng lạc quan, tràn đầy nhiệt huyết, việc học việc làm luôn xuất sắc nhé! 💚💪",
+    text: "Chúc Kim Thi lúc nào cũng lạc quan, tràn đầy nhiệt huyết, học tập và công việc luôn đạt kết quả thật xuất sắc nhé! 💚💪",
     img: "./assets/kt7.png",
   },
   {
-    text: "Chúc Kim Thi có một đêm Trung Thu thật chill, ngắm trăng phá cỗ và luôn tỏa sáng rực rỡ nha! 🏮✨",
+    text: "Chúc Kim Thi có một đêm Trung Thu thật thảnh thơi, ngắm trăng phá cỗ và nụ cười luôn tỏa sáng rực rỡ nha! 🏮✨",
     img: "./assets/kt8.png",
   },
   {
-    text: "Chúc Kim Thi mỗi ngày trôi qua đều là một ngày bình yên, vui vẻ và nhận được thật nhiều yêu thương! 🥑🍀",
+    text: "Chúc Kim Thi mỗi ngày trôi qua đều bình yên, vui tươi và luôn nhận được thật nhiều yêu thương, quý mến! 🌸🍀",
     img: "./assets/kt9.png",
   },
   {
-    text: "Chúc Kim Thi luôn xinh đẹp tựa như nàng tiên ánh trăng, tâm an yên và nụ cười luôn nở trên môi! 🌙💖",
+    text: "Chúc Kim Thi luôn xinh xắn, rạng ngời như ánh trăng rằm, tâm hồn an yên và nụ cười luôn rạng rỡ trên môi! 🌙✨",
     img: "./assets/kt10.png",
   },
 ];
